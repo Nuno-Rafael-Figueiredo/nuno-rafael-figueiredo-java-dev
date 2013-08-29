44fbd25ec917773045231b87151a00402ee18190
nuno.rafael.figueiredo@gmail.com <nuno.rafael.figueiredo@gmail.com@5eaec24d-0d66-896f-66f0-31a4d9e3933d>
2013-08-29 16:09:42 +0000

    Design + MySQL + MySQL Sakila Sample Database Model & Tests
    Dependencies resolved

---
+++ Employees Model/src/employees/model/tables/Offices.java
@@ -0,0 +1,11 @@
+package employees.model.tables;
+
+import mysql.Table;
+
+/**
+ * User: Nuno
+ * Date: 28-08-2013
+ * Time: 15:46
+ */
+public class Offices extends Table {
+}
---
+++ Employees Model/src/employees/model/tables/ProductLines.java
@@ -0,0 +1,16 @@
+package employees.model.tables;
+
+import mysql.Column;
+import mysql.Table;
+import mysql.dataTypes.Varchar;
+
+/**
+ * User: Nuno
+ * Date: 28-08-2013
+ * Time: 15:46
+ */
+public class ProductLines extends Table {
+    public Column productLine(){
+        return createColumn("productLine", new Varchar(50), Column.Property.notNull, Column.Property.primaryKey);
+    }
+}

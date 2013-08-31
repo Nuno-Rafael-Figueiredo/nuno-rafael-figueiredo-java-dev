169d572824d4f08196ac2944dbfa1f492dcb76bb
nuno.rafael.figueiredo@gmail.com <nuno.rafael.figueiredo@gmail.com@5eaec24d-0d66-896f-66f0-31a4d9e3933d>
2013-08-31 22:21:44 +0000

    Employees Model - Adding Tables

---
+++ Design/src/design/patterns/state/MementoRestored.java
@@ -0,0 +1,11 @@
+package design.patterns.state;
+
+/**
+ * User: Nuno
+ * Date: 31-08-2013
+ * Time: 23:13
+ */
+public class MementoRestored {
+    public MementoRestored(Context context, Context.Memento memento) {
+    }
+}
